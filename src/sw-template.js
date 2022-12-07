f7827e112ece164e4ec1dce15d89c52a51df50c5
Diego Pagini <diegopaginidev@gmail.com>
2022-12-07 15:56:09 +0100

    NetworkFirst

---
+++ src/sw-template.js
@@ -0,0 +1,29 @@
+/** @format */
+
+// eslint-disable-next-line no-undef
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+// eslint-disable-next-line no-restricted-globals, no-undef
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+// eslint-disable-next-line no-undef
+const { registerRoute } = workbox.routing;
+// eslint-disable-next-line no-undef
+const { CacheFirst, NetworkFirst } = workbox.strategies;
+
+registerRoute(
+	new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
+	new CacheFirst()
+);
+
+registerRoute(
+	new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'),
+	new CacheFirst()
+);
+
+registerRoute(
+	new RegExp('https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'),
+	new CacheFirst()
+);
+
+registerRoute(new RegExp('http://localhost:4000/api/auth/renew'), new NetworkFirst());
+
+registerRoute(new RegExp('http://localhost:4000/api/events'), new NetworkFirst());
--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,9 @@
+/** @format */
+
 module.exports = {
 	globDirectory: 'build/',
-	globPatterns: [
-		'**/*.{json,ico,html,png,txt,css,js}'
-	],
+	globPatterns: ['**/*.{json,ico,html,png,txt,css,js}'],
 	swDest: 'build/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+	// ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+	swSrc: 'src/sw-template.js',
 };
