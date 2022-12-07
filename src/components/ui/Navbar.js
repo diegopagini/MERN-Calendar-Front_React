ab81325a547d570893fa5c1da2f7c8b6fa250f99
Diego Pagini <diegopaginidev@gmail.com>
2022-12-07 16:53:07 +0100

    react-detect-offline

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,7 +1,8 @@
 /** @format */
-
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { Offline, Online } from 'react-detect-offline';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { startLogout } from '../../actions/auth';
 
 export const Navbar = () => {
@@ -15,6 +16,14 @@ export const Navbar = () => {
 	return (
 		<div className='navbar navbar-dark bg-dark mb-4'>
 			<span className='navbar-brand'>{name}</span>
+
+			<Online>
+				<span className='text-success'>Online</span>
+			</Online>
+			<Offline>
+				<span className='text-danger'>Offline</span>
+			</Offline>
+
 			<button onClick={handleLogout} className='btn btn-outline-danger'>
 				<i className='fas fa-sign-out-alt'></i>
 				<span> Salir</span>
