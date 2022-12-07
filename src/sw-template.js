94841427c90304ed9add4a052dbe088385b477da
Diego Pagini <diegopaginidev@gmail.com>
2022-12-07 16:47:11 +0100

    SW

--- src/sw-template.js
+++ src/sw-template.js
@@ -13,26 +13,24 @@ const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 // eslint-disable-next-line no-undef
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
-registerRoute(
-	new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
-	new CacheFirst()
-);
+// Network First.
+const cacheNetworkFirst = ['/api/auth/renew', '/api/events'];
+registerRoute(({ url }) => {
+	if (cacheNetworkFirst.includes(url.pathname)) return true;
+	return false;
+}, new NetworkFirst());
 
-registerRoute(
-	new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'),
-	new CacheFirst()
-);
+// Cache first.
+const cacheFirstNetwork = [
+	'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
+	'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
+	'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
+];
+registerRoute(({ url }) => {
+	if (cacheFirstNetwork.includes(url.href)) return true;
+}, new CacheFirst());
 
-registerRoute(
-	new RegExp('https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'),
-	new CacheFirst()
-);
-
-registerRoute(new RegExp('http://localhost:4000/api/auth/renew'), new NetworkFirst());
-
-registerRoute(new RegExp('http://localhost:4000/api/events'), new NetworkFirst());
-
-// POST Offline
+// POST Offline.
 const bgSyncPlugin = new BackgroundSyncPlugin('offline-crud', {
 	maxRetentionTime: 24 * 60,
 });
@@ -45,7 +43,7 @@ registerRoute(
 	'POST'
 );
 
-// DELETE Offline
+// DELETE Offline.
 registerRoute(
 	new RegExp('http://localhost:4000/api/event/'),
 	new NetworkOnly({
@@ -54,7 +52,7 @@ registerRoute(
 	'DELETE'
 );
 
-// PUT Offline
+// PUT Offline.
 registerRoute(
 	new RegExp('http://localhost:4000/api/event/'),
 	new NetworkOnly({
